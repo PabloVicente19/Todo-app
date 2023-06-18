915cb317dfcb8f8037ba53893d130fbae82e4c13
Pablo Vicente <pablovicente19@hotmail.com>
2023-06-18 15:51:57 -0300

    seguir con la logica de la todo

--- src/js/index.js
+++ src/js/index.js
@@ -12,22 +12,24 @@ const saveLocalStorage = (taskList) => {
 
 // Creo la card de la tarea
 const createTaskCard = (tasks) => {
-  const {name} = tasks;
-  return `<li> ${name} </li>`
+  const {name, id} = tasks;
+  return `<li class="todo-task"> ${name} <i class="fa-solid fa-trash icon-remove-task" data-id=${id}></i></li>`
 }
 
 // funcion para renderizal las tareas
-const renderCards = (container,task) => {
-  return container.innerHTML += task.map(createTaskCard).join("");
+const renderCards = (container) => {
+  return container.innerHTML = task.map(createTaskCard).join("");
 }
-
+// Obtengo el dato del input, lo guado en LS y lo renderizo
 $form.addEventListener('submit', (e) => {
   e.preventDefault()
 
   let taskName = $taskInput.value.trim();
-  
-  task = [...task, {name: taskName}]
-  
-  console.log(taskName);
-  console.log(task);
+  // Le doy formato a la task
+  task = [...task, {id:task.length + 1, name: taskName}]
+  // Almaceno la task
+  saveLocalStorage(task);
+  renderCards($taskCoontainer);
+
+
 })
